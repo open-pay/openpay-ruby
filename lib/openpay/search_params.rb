aa23220c48e1b23399df702c076d27f66135c53f
ronniebermejo <ronnie.bermejo.mx@gmail.com>
2014-06-12 15:02:20 -0500

    Add list methods and its related specs. Some failing cases were fixed. 3 Payout cases are still failing.

---
+++ lib/openpay/search_params.rb
@@ -0,0 +1,20 @@
+module OpenpayUtils
+
+  class SearchParams < OpenStruct
+
+    #"?creation[gte]=2013-11-01&limit=2&amount[lte]=100"
+    def to_str
+      filter = '?'
+      self.marshal_dump.each do |attribute, value|
+        if attribute =~ /(\w+)_(gte|lte)/
+          attribute = "#{$1}[#{$2}]"
+        end
+        filter << "#{attribute}=#{value}&"
+      end
+      filter.sub(/\&$/, '')
+    end
+
+    alias :to_s :to_str
+
+  end
+end
