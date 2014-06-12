e36959052da315987430f4eb0a2e708aa74e846d
ronniebermejo <ronnie.bermejo.mx@gmail.com>
2014-06-12 14:27:13 -0500

    SearchParams spec

---
+++ test/spec/utils/search_params_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../../spec_helper'
+include OpenpayUtils
+
+describe OpenpayUtils do
+
+  describe SearchParams do
+
+    subject(:search_params) { SearchParams.new }
+
+    describe 'setters and getters' do
+
+      it 'sets and gets a given value' do
+        val = "val"
+        search_params.val = val
+        expect(search_params.val).to eq(val)
+      end
+
+      it 'sets and gets a non existing attribute' do
+        expect(search_params.val).to eq(nil)
+      end
+    end
+
+    describe '.to_s' do
+      it 'generates the filter string based on the attributes' do
+        creation_date = "2013-11-01"
+        search_params.creation_gte = creation_date
+        limit = 2
+        search_params.limit = limit
+        amount = 100
+        search_params.amount_lte = amount
+        expect(search_params.to_s).to eq("?creation[gte]=2013-11-01&limit=2&amount[lte]=100")
+      end
+    end
+  end
+end
+
