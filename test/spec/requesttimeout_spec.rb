843f683987a53086219fe2a7dfa72e565c4d6225
Marcos Coronado <tonik_87@hotmail.com>
2014-08-04 12:06:08 -0500

    Se modifica el test, solo para que obtenga los cargos y se pueda validar el uso de la nueva firma del metido initialize

--- test/spec/requesttimeout_spec.rb
+++ test/spec/requesttimeout_spec.rb
@@ -1,35 +1,23 @@
 require_relative '../spec_helper'
 
 
-describe Bankaccounts do
+describe Charges do
 
-  #bankaccounts for merchant cannot be created using the api
-  #the merchant bank account should be created using the Openpay dashboard
   before(:all) do
 
     @merchant_id='mywvupjjs9xdnryxtplq'
     @private_key='sk_92b25d3baec149e6b428d81abfe37006'
 
-    @openpay=OpenpayApi.new(@merchant_id, @private_key, false, 30)
-    @bank_accounts=@openpay.create(:bankaccounts)
-    @customers=@openpay.create(:customers)
+    @openpay=OpenpayApi.new(@merchant_id, @private_key, false, 5)
+    @charges=@openpay.create(:charges)
 
   end
 
-  describe '.get' do
+  describe '.all' do
 
-    it 'get a given bank account for a given customer' do
+    it 'get all charges' do
 
-      customer_hash= FactoryGirl.build(:customer)
-      customer=@customers.create(customer_hash)
-
-      account_hash=FactoryGirl.build(:bank_account)
-      bank=@bank_accounts.create(account_hash, customer['id'])
-
-      bank_account=@bank_accounts.get(customer['id'], bank['id'])
-      expect(bank_account['alias']).to match 'Cuenta principal'
-      @bank_accounts.delete(customer['id'], bank['id'])
-      @customers.delete(customer['id'])
+      @charges.all
 
     end
 
